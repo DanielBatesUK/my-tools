3ab87940f0d09cb97ed1cdaaae59841b61685dc3
DanielBatesUK <email@danielbates.co.uk>
2022-09-23 17:05:14 +0100

    Updating webhook

--- routes/github_webhook.js
+++ routes/github_webhook.js
@@ -2,7 +2,7 @@
 
 // Imports
 import crypto from 'crypto';
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 
 // ################################################################################################
 
@@ -13,11 +13,27 @@ function routeGitHubWebhook(req, res) {
     const secret = process.env.SESSION_SECRET;
     const expectedSignature = `sha1=${crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex')}`;
     const receivedSignature = req.headers['x-hub-signature'];
+    // Check Webhook signature
     if (receivedSignature !== expectedSignature) throw Error('Signature mismatch from GitHub Webhook');
     console.log('Signature from GitHub Webhook verified successfully');
     console.log('GitHub Webhook', req.body);
     res.status(200).send('{"status":200}');
     res.end();
+    // Check GitHub branch
+    const localPath = process.cwd();
+    const localBranch = execSync(`cd ${localPath} && git rev-parse --abbrev-ref HEAD`).toString().trim();
+    const gitHubPushBranch = req.body.ref.split('/')[2];
+    const gitHubDefaultBranch = req.body.repository.default_branch;
+    console.log({ localPath, localBranch, gitHubPushBranch, gitHubDefaultBranch });
+    // Pull GitHub branch (if default branch)
+    if (localBranch === gitHubDefaultBranch && localBranch === gitHubPushBranch) {
+      console.log(`Default branch check successful: '${localBranch}'`);
+      console.log(`Executing 'git pull' in '${localPath}'`);
+      const gitPull = execSync(`cd ${localPath} && git pull`);
+      console.log({ gitPull });
+    } else {
+      console.log('Default branch check mismatch:', { localBranch, gitHubPushBranch, gitHubDefaultBranch });
+    }
   } catch (error) {
     console.error(error);
     res.status(403).send(`{"github_webhook: {"error": "${error}"}`);
