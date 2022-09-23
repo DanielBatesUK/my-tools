771f1a2888af5c4522af2082bdc9a89ac556b863
DanielBatesUK <email@danielbates.co.uk>
2022-09-23 14:31:18 +0100

    Updates

--- routes/github_webhook.js
+++ routes/github_webhook.js
@@ -1,15 +1,26 @@
 // ################################################################################################
 
-// Route - Index
+// Imports
+import crypto from 'crypto';
+import { exec } from 'child_process';
+
+// ################################################################################################
+
+// Route - GitHub Webhooks
 function routeGitHubWebhook(req, res) {
   try {
     console.log(`Processing HTTP ${req.method} request for '${req.path}' as 'GitHub Webhook'`);
-    console.log('Github Webhook', req.body);
+    const secret = process.env.SESSION_SECRET;
+    const expectedSignature = `sha1=${crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex')}`;
+    const receivedSignature = req.headers['x-hub-signature'];
+    if (receivedSignature !== expectedSignature) throw Error('Signature mismatch from GitHub Webhook');
+    console.log('Signature from GitHub Webhook verified successfully');
+    console.log('GitHub Webhook', req.body);
     res.status(200).send('{"status":200}');
     res.end();
   } catch (error) {
     console.error(error);
-    res.send('Index error');
+    res.status(403).send(`{"github_webhook: {"error": "${error}"}`);
     res.end();
   }
 }
