6cb5a46777a3d2bd67f241872a7e3a299c1c02d6
Daniel Bates <64318815+DanielBatesUK@users.noreply.github.com>
2022-09-24 20:18:40 +0100

    Prettier'd

--- routes/github_webhook.js
+++ routes/github_webhook.js
@@ -6,15 +6,20 @@ import { execSync } from 'child_process';
 
 // ################################################################################################
 
+// Check webhook signature
+function checkSignature(receivedSignature, body) {
+  const secret = process.env.GITHUB_WEBHOOK_SECRET;
+  const expectedSignature = `sha1=${crypto.createHmac('sha1', secret).update(JSON.stringify(body)).digest('hex')}`;
+  return receivedSignature === expectedSignature ? true : false;
+}
+
 // Route - GitHub Webhooks
 function routeGitHubWebhook(req, res) {
   try {
     console.log(`Processing HTTP ${req.method} request for '${req.path}' as 'GitHub Webhook'`);
-    const secret = process.env.GITHUB_WEBHOOK_SECRET;
-    const expectedSignature = `sha1=${crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex')}`;
-    const receivedSignature = req.headers['x-hub-signature'];
     // Check Webhook signature
-    if (receivedSignature !== expectedSignature) throw Error('Signature mismatch from GitHub Webhook');
+    if (checkSignature(req.headers['x-hub-signature'], req.body) === false)
+      throw Error('Signature mismatch from GitHub Webhook');
     console.log('Signature from GitHub Webhook verified successfully');
     console.log('GitHub Webhook', req.body);
     res.status(200).send('{"status":200}');
@@ -24,7 +29,12 @@ function routeGitHubWebhook(req, res) {
     const localBranch = execSync(`cd ${localPath} && git rev-parse --abbrev-ref HEAD`).toString().trim();
     const gitHubPushBranch = req.body.ref.split('/')[2];
     const gitHubDefaultBranch = req.body.repository.default_branch;
-    console.log({ localPath, localBranch, gitHubPushBranch, gitHubDefaultBranch });
+    console.log({
+      localPath,
+      localBranch,
+      gitHubPushBranch,
+      gitHubDefaultBranch,
+    });
     // Pull GitHub branch (if default branch)
     if (localBranch === gitHubDefaultBranch && localBranch === gitHubPushBranch) {
       console.log(`Default branch check successful: '${localBranch}'`);
@@ -42,7 +52,11 @@ function routeGitHubWebhook(req, res) {
         console.log('Refresh command failed (are you on Glitch?)');
       }
     } else {
-      console.log('Default branch check mismatch:', { localBranch, gitHubPushBranch, gitHubDefaultBranch });
+      console.log('Default branch check mismatch:', {
+        localBranch,
+        gitHubPushBranch,
+        gitHubDefaultBranch,
+      });
     }
   } catch (error) {
     console.error(error);
--- routes/hashes.js
+++ routes/hashes.js
@@ -15,10 +15,10 @@ function alternateCase(string) {
   let results = '';
   let capsCounter = 0;
   for (let i = 0; i < string.length; i += 1) {
-    const char = string.substring(i, (i + 1));
-    if ((/[a-zA-Z]/).test(char) === true) {
+    const char = string.substring(i, i + 1);
+    if (/[a-zA-Z]/.test(char) === true) {
       // Is letter: so set case
-      results += ((capsCounter % 2 === 0) ? char.toLowerCase() : char.toUpperCase());
+      results += capsCounter % 2 === 0 ? char.toLowerCase() : char.toUpperCase();
       capsCounter += 1;
     } else {
       // Is not a letter: add to results
@@ -29,9 +29,9 @@ function alternateCase(string) {
 }
 
 function lowerUpperNumbers(string) {
-  const hasLower = (/[a-z]/).test(string);
-  const hasUpper = (/[A-Z]/).test(string);
-  const hasNumbers = (/[0-9]/).test(string);
+  const hasLower = /[a-z]/.test(string);
+  const hasUpper = /[A-Z]/.test(string);
+  const hasNumbers = /[0-9]/.test(string);
   let results = string;
 
   // Check string
@@ -45,15 +45,28 @@ function lowerUpperNumbers(string) {
   if (!hasLower) {
     console.info(`fixString - String has no lowercase letters: ${results}`);
     // No lowercase letters found - change last character
-    if ((/[a-zA-Z]/).test(results.substring(results.length - 1, results.length))) {
+    if (/[a-zA-Z]/.test(results.substring(results.length - 1, results.length))) {
       // Last character is a letter - change to lowercase
-      console.info(`fixString - last character is a letter '${results.substring(results.length - 1, results.length)}' change to lowercase: ${results}`);
-      results = results.substring(0, results.length - 1) + results.substring(results.length - 1, results.length).toLowerCase();
+      console.info(
+        `fixString - last character is a letter '${results.substring(
+          results.length - 1,
+          results.length
+        )}' change to lowercase: ${results}`
+      );
+      results =
+        results.substring(0, results.length - 1) + results.substring(results.length - 1, results.length).toLowerCase();
     } else {
       // Last character is a number - create lowercase from number
       let lastNumber = Number(results.substring(results.length - 1, results.length));
       if (lastNumber > 5) lastNumber = Math.round(lastNumber / 2);
-      console.info(`fixString - Last character is a number '${results.substring(results.length - 1, results.length)}' change to lowercase character-code '${lastNumber + 65 + 32}' (${String.fromCharCode(lastNumber + 65 + 32)}): ${string}`);
+      console.info(
+        `fixString - Last character is a number '${results.substring(
+          results.length - 1,
+          results.length
+        )}' change to lowercase character-code '${lastNumber + 65 + 32}' (${String.fromCharCode(
+          lastNumber + 65 + 32
+        )}): ${string}`
+      );
       results = results.substring(0, results.length - 1) + String.fromCharCode(lastNumber + 65 + 32);
     }
     // Check string again
@@ -65,15 +78,21 @@ function lowerUpperNumbers(string) {
   if (!hasUpper) {
     console.info(`fixString - String has no uppercase letters: ${results}`);
     // No uppercase letters found - change first character
-    if ((/[a-zA-Z]/).test(results.substring(0, 1))) {
+    if (/[a-zA-Z]/.test(results.substring(0, 1))) {
       // First character is a letter - change to uppercase
-      console.info(`fixString - First character is a letter '${results.substring(0, 1)}' change to uppercase: ${results}`);
+      console.info(
+        `fixString - First character is a letter '${results.substring(0, 1)}' change to uppercase: ${results}`
+      );
       results = results.substring(0, 1).toUpperCase() + results.substring(1, results.length);
     } else {
       // First character is a number - create uppercase from number
       let firstNumber = Number(results.substring(0, 1));
       if (firstNumber > 5) firstNumber /= 2;
-      console.info(`fixString - First character is a number '${results.substring(0, 1)}' change to uppercase character-code '${firstNumber + 65}' (${String.fromCharCode(firstNumber + 65)}): ${results}`);
+      console.info(
+        `fixString - First character is a number '${results.substring(0, 1)}' change to uppercase character-code '${
+          firstNumber + 65
+        }' (${String.fromCharCode(firstNumber + 65)}): ${results}`
+      );
       results = String.fromCharCode(firstNumber + 65).toUpperCase() + results.substring(1, results.length);
     }
     console.info(`fixString - Replaced first character with uppercase letter: ${results}`);
@@ -85,7 +104,11 @@ function lowerUpperNumbers(string) {
   if (!hasNumbers) {
     console.info(`fixString - String has no numbers: ${results}`);
     // No numbers found - change second character
-    console.info(`fixString - Second character (${results.substring(1, 2)}) is character-code '${results.charCodeAt(1)}' change letter to number '${(results.charCodeAt(1) - 65)}': ${results}`);
+    console.info(
+      `fixString - Second character (${results.substring(1, 2)}) is character-code '${results.charCodeAt(
+        1
+      )}' change letter to number '${results.charCodeAt(1) - 65}': ${results}`
+    );
     results = results.substring(0, 1) + (results.charCodeAt(1) - 65) + results.substring(2, results.length);
     console.info(`fixString - Replaced second character with number: ${results}`);
     // Check string again
@@ -98,7 +121,40 @@ function lowerUpperNumbers(string) {
 
 function addSymbols(string) {
   // Array of text symbols used
-  const symbolsArray = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+  const symbolsArray = [
+    '!',
+    '"',
+    '#',
+    '$',
+    '%',
+    '&',
+    "'",
+    '(',
+    ')',
+    '*',
+    '+',
+    ',',
+    '-',
+    '.',
+    '/',
+    ':',
+    ';',
+    '<',
+    '=',
+    '>',
+    '?',
+    '@',
+    '[',
+    '\\',
+    ']',
+    '^',
+    '_',
+    '`',
+    '{',
+    '|',
+    '}',
+    '~',
+  ];
 
   // Get first number in the string
   let firstNumber = Number(string[string.search(/[0-9]/)]);
@@ -111,14 +167,27 @@ function addSymbols(string) {
   // Replace first character in each part with symbol
   let results = '';
   for (let i = 0; i < stringArray.length; i += 1) {
-    results = results + stringArray[i].substring(0, stringArray[i].length - 1) + symbolsArray[(((firstNumber += 1) * (i + 1)) % symbolsArray.length)];
-    console.info(`addSymbols - Replaced last character with symbol '${symbolsArray[((firstNumber * (i + 1)) % symbolsArray.length)]}' on string part #${i + 1} '${stringArray[i]}': ${string}`);
+    results =
+      results +
+      stringArray[i].substring(0, stringArray[i].length - 1) +
+      symbolsArray[((firstNumber += 1) * (i + 1)) % symbolsArray.length];
+    console.info(
+      `addSymbols - Replaced last character with symbol '${
+        symbolsArray[(firstNumber * (i + 1)) % symbolsArray.length]
+      }' on string part #${i + 1} '${stringArray[i]}': ${string}`
+    );
   }
   // Finished
   return results;
 }
 
-function getHash(string, outputLength = 32, forceAlternateCase = true, forceLowerUpperNumbers = true, forceSymbols = true) {
+function getHash(
+  string,
+  outputLength = 32,
+  forceAlternateCase = true,
+  forceLowerUpperNumbers = true,
+  forceSymbols = true
+) {
   // Cut size of string
   let results = String(getMD5(string)).substring(0, outputLength);
   // Force alternating case
@@ -126,7 +195,7 @@ function getHash(string, outputLength = 32, forceAlternateCase = true, forceLowe
   // Force Lower, Upper & numbers
   if (forceLowerUpperNumbers) results = lowerUpperNumbers(results);
   // Force symbols
-  if (forceSymbols) results = (results.length > 7 ? addSymbols(results) : '{ error: string length must >= 8 }');
+  if (forceSymbols) results = results.length > 7 ? addSymbols(results) : '{ error: string length must >= 8 }';
 
   // Finished
   return results;
@@ -142,7 +211,9 @@ function hashTableData(inputString) {
     if (Math.sign(i) === 1) ojectMonthName = `p${i}_month`;
     const tempDate = new Date(currentDate.getTime());
     tempDate.setMonth(tempDate.getMonth() + i);
-    const tempString = String(inputString + tempDate.toLocaleString('default', { month: 'long' }) + String(tempDate.getFullYear()));
+    const tempString = String(
+      inputString + tempDate.toLocaleString('default', { month: 'long' }) + String(tempDate.getFullYear())
+    );
     results[ojectMonthName] = {
       heading: String(tempDate.getFullYear()).substring(2, 4) + String(tempDate.getMonth() + 1).padStart(2, '0'),
       hash_8: getHash(tempString, 8, true, true, true),
@@ -157,7 +228,7 @@ function hashTableData(inputString) {
 function spanTitles(string) {
   let results = '';
   for (let i = 0; i < string.length; i += 1) {
-    results = `${results}<span title='${(i + 1)}'>${string[i]}</span>`;
+    results = `${results}<span title='${i + 1}'>${string[i]}</span>`;
   }
   // Finished
   return results;
@@ -169,7 +240,7 @@ function spanTitles(string) {
 function routeIndex(req, res) {
   try {
     console.log(`Processing HTTP ${req.method} request for '${req.path}' as 'hashes'`);
-    const inputString = String((req.body.inputString ? req.body.inputString : '')).toLowerCase();
+    const inputString = String(req.body.inputString ? req.body.inputString : '').toLowerCase();
     const pageOptions = {
       input_string: inputString,
       full_md5: getHash(inputString, 32, false, false, false),
@@ -183,7 +254,9 @@ function routeIndex(req, res) {
       hash_table: hashTableData(inputString),
     };
     // Span Titles
-    Object.keys(pageOptions).forEach((key) => { pageOptions[`span_title_${key}`] = spanTitles(pageOptions[key]); });
+    Object.keys(pageOptions).forEach((key) => {
+      pageOptions[`span_title_${key}`] = spanTitles(pageOptions[key]);
+    });
     res.render('hashes', pageOptions);
     res.end();
   } catch (error) {
