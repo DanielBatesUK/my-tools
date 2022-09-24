56b305d06ff0768dbbfc216e1e0a2f288984ca55
Daniel Bates <64318815+DanielBatesUK@users.noreply.github.com>
2022-09-24 20:34:36 +0100

    Testing glitch webhooks

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ function routeIndex(req, res) {
     console.log(`Processing HTTP ${req.method} request for '${req.path}' as 'index'`);
     const pageOptions = {
       page_title: 'Index',
-      page_content: 'Hello World!',
+      page_content: 'Hello World! Testing webhooks',
     };
     res.render('index', pageOptions);
     res.end();
