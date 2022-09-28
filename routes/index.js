8e0d8ed440f0433b5f5437fd5af85215a7aa131d
DanielBatesUK <email@danielbates.co.uk>
2022-09-28 13:21:47 +0100

    Change index to link to the other tools

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@
 function routeIndex(req, res) {
   try {
     console.log(`Processing HTTP ${req.method} request for '${req.path}' as 'index'`);
-    const pageOptions = {
-      page_title: 'Index',
-      page_content: 'Hello World!',
-    };
-    res.render('index', pageOptions);
+    res.render('index');
     res.end();
   } catch (error) {
     console.error(error);
